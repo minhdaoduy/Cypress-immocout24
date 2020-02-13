8480a828172c8cb91d2523031fd5a21ee8a2c758
Minh Dao <daoduyminh@gmail.com>
2020-02-13 11:41:13 +0700

    no message

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,18 +1,47 @@
-let chai = require('chai');
-let expect = chai.expect;
-// let cy = require('cypress');
-// let cy = cypress.
+// let chai = require('chai');
+// let expect = chai.expect;
+// let Cypress = require('../../node_modules/cypress/types/index.d.ts');
+// let cy = Cypress.cy;
 
 describe('My First Test', function() {
+    let text;
 
 
     it('Does not do much!', async function() {
+        debugger
         expect(true).to.equal(true)
     });
 
     it('Visits the Kitchen Sink', async function() {
         await cy.visit('/');
-        cy.get()
-        // cy.log("Minh test");
+        let element  = cy.get('h1', { timeout: 10000}).debug();
+        await element.should('have.text', 'Kitchen Sink');
+        // text = await Chainable.get('h1', { timeout: 10000}).its('text()');
+    });
+
+    it('verify text', async function() {
+        expect('Kitchen Sink').to.equal(text)
     })
 });
+//
+// module.exports = (on, config) => {
+//
+//     on('before:browser:launch', (browser = {}, args) => {
+//
+//     it('Does not do much!', async function() {
+//         expect(true).to.equal(true)
+//     });
+//
+//     it('Visits the Kitchen Sink', async function() {
+//         await cy.visit('/');
+//         let element  = cy.get('h1', { timeout: 10000}).debug();
+//         await element.should('have.text', 'Kitchen Sink');
+//         // text = await Chainable.get('h1', { timeout: 10000}).its('text()');
+//     });
+//
+//     it('verify text', async function() {
+//         expect('Kitchen Sink').to.equal(text)
+//     })
+//
+//     })
+//   }
